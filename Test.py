f993778115223d63a7740da9f5a912b81be72e27
vinnowin <vincenthuf@live.nl>
2024-10-01 15:21:27 +0200

    Werkend voor eclipse data, maar nog zonder random forest en bagging

--- Test.py
+++ Test.py
@@ -30,15 +30,27 @@ def pre_process_indian(data):
     features = data.drop('class', axis=1).to_numpy()
     return features, classification
 
+def eclipse(file):
+    df = pd.read_csv(file, sep = ';')
+
+    # dit zijn vgm de predictors die we moeten gebruiken (zie tabel uit artikel). Het zijn er ook 41 dus dat klopt 
+    predictors = ['pre','FOUT_avg', 'FOUT_max', 'FOUT_sum', 'MLOC_avg', 'MLOC_max', 'MLOC_sum', 'NBD_avg', 'NBD_max', 'NBD_sum', 'NOF_avg', 'NOF_max', 'NOF_sum', 'NOM_avg', 'NOM_max', 'NOM_sum', 'NSF_avg', 'NSF_max', 'NSF_sum', 'NSM_avg', 'NSM_max', 'NSM_sum', 'PAR_avg', 'PAR_max', 'PAR_sum', 'VG_avg', 'VG_max', 'VG_sum', 'NOCU', 'ACD_avg', 'ACD_max', 'ACD_sum', 'NOI_avg', 'NOI_max', 'NOI_sum', 'NOT_avg', 'NOT_max', 'NOT_sum', 'TLOC_avg', 'TLOC_max', 'TLOC_sum']
+    target = 'post'
+    df[target] = df[target].astype(int)
+
+    # Change target variables such that it indicates whether post bugs are found or not (instead of how many)
+    df.loc[df[target] > 0, target] = 1
+    variables = df[predictors].to_numpy()
+    target = df[target].to_numpy()
+    return variables, target
+
+
 def evaluate(real_class, predictions):
-    cm = confusion_matrix(real_class, predictions)
-    
-    # Create a DataFrame for better readability
+    cm = confusion_matrix(real_class, predictions) 
     cm_df = pd.DataFrame(cm)
     return cm_df
 
-def metrics(y_test_labels, y_pred_labels):
-        
+def metrics(y_test_labels, y_pred_labels): 
     # Bereken de metrics voor de Decision Tree (1=pos en 0=neg)
     accuracy = accuracy_score(y_test_labels, y_pred_labels)
     precision = precision_score(y_test_labels, y_pred_labels)
@@ -54,20 +66,14 @@ def metrics(y_test_labels, y_pred_labels):
 
 if __name__ == "__main__":
     # Load dataset
-    data = data = np.genfromtxt('indian.txt', delimiter=',')
-
-    # Pre-process data to optain features (x) and labels (y)
-    x, y = pre_process_indian(data)
+    x_train, y_train = eclipse('eclipse_train.csv')
+    x_test, y_test = eclipse('eclipse_test.csv')
 
     # Initiate DecisionTree with nmin, minleaf, en nfeat
-    tree = DecisionTree(nmin=20, minleaf=5, nfeat=8)
+    tree = DecisionTree(nmin=15, minleaf=5, nfeat=41)
 
     # Fit the decision tree 
-    tree.fit(x, y)
-
-    # Nieuwe data waarvoor we voorspellingen willen doen
-    new_data = x
-    new_data_predictions = y
+    tree.fit(x_train, y_train)
     
     # Je zou nu de tree structuur kunnen visualiseren of traverseren om te zien of de boom goed is gegroeid
     # def traverse_tree(node, depth=0):
@@ -83,13 +89,9 @@ if __name__ == "__main__":
     # traverse_tree(tree.root)
 
     # Voorspellingen maken met de getrainde boom
-    predictions = tree_pred(new_data, tree.root)
-
+    predictions = tree_pred(x_test, tree.root)
     print("Predicted class labels:", predictions)
-    print("dit is labels: ", y)
-    print(" dit is predictions: ", predictions)
-    metrics = metrics(y, predictions)
+    metrics = metrics(y_test, predictions)
     print("metrics: ", metrics)
-    #evaluations = evaluate(new_data_predictions, predictions)
-    #print("Predicted class labels:", predictions)
-    #print(evaluations)
+    evaluations = evaluate(y_test, predictions)
+    print(evaluations)
